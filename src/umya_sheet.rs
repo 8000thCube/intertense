8654e96aedb76424d0c85d0b1cdbea59b33799d9
Aili Nierengarten <125620224+8000thCube@users.noreply.github.com>
2025-12-15 04:35:04 -0800

    Update umya_sheet.rs

--- src/umya_sheet.rs
+++ src/umya_sheet.rs
@@ -61,7 +61,7 @@ impl CellPattern{
 			CellPattern::Bool(e)=>if let Ok(x)=s.parse::<bool>(){((e^x) as usize) as f32*boolscale}else{parsescale},
 			CellPattern::Boolean=>if let Ok(_x)=s.parse::<bool>(){0.0}else{parsescale},
 			CellPattern::Nothing=>if s.len()==0{0.0}else{parsescale},
-			CellPattern::Number(e)=>if let Ok(x)=s.parse::<f64>(){((e-x)*diffscale as f64) as f32}else{parsescale},
+			CellPattern::Number(e)=>if let Ok(x)=s.parse::<f64>(){((e-x).abs()*diffscale as f64) as f32}else{parsescale},
 			CellPattern::Numeric=>if let Ok(_x)=s.parse::<f64>(){0.0}else{parsescale},
 			CellPattern::Text(z)=>METRIC.get_or_init(Levenshtein::new).distance(s,z) as f32*levscale
 		}
