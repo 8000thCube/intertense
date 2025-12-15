c054780862e30274ac95a8a4a4038f6c27388db3
Aili Nierengarten <125620224+8000thCube@users.noreply.github.com>
2025-12-15 15:48:07 -0800

    Add files via upload

---
+++ match_tensor.rs
@@ -0,0 +1,218 @@
+#[cfg(test)]
+mod tests{
+	#[test]
+	fn absorb_00(){
+		let offsetcandidates=GridIter::new([2,3]);
+		let mut data:Tensor<f32>=vec![
+			0.5,0.0,0.5,0.4,
+			0.4,0.1,0.4,0.4,
+			0.3,0.2,0.4,0.5,
+		].into();
+		let mut query:Tensor<f32>=vec![	// approximately this appears at 1,1
+			0.2,0.4,
+			0.2,0.4,
+		].into();
+
+		assert!(data.reshape([3,4])&&query.reshape([2,2]));
+		let (value, cost)=absorb_data(data.view(),|x0,x1|x0.iter().zip(x1.iter()).map(|(x0,x1)|(x0-x1) as f32).map(|x|x*x).sum::<f32>().sqrt(),offsetcandidates,|a,b|(a-b).abs(),query.view()).unwrap();
+
+		assert_eq!((cost,value.flat_vec(None)),(0.1,vec![0.1,0.4,0.2,0.4]));
+	}
+	#[test]
+	fn fill_holes_00(){
+		let mut data:Tensor<f32>=vec![
+			0.5,0.0,0.5,0.4,0.1,
+			0.0,0.1,0.0,0.4,0.2,
+			0.0,0.0,0.4,0.0,0.3,
+		].into();
+		let filled=vec![
+			0.5,0.0,0.5,0.4,0.1,
+			0.5,0.1,0.5,0.4,0.2,
+			0.5,0.1,0.4,0.4,0.3,
+		];
+
+		assert!(data.reshape([3,5]));
+		fill_holes(data.view_mut(),0,|&x|x,|&x|x==0.0);
+
+		assert_eq!(data.flat_vec(None),filled);
+	}
+
+	use super::*;
+}
+/// TODO might not quite work as expected find the lowest cost offset from the candidates and extract the corresponding components from the data. returns none if length of offsetcandidates is 0. having any offset candidates that put any of query outside of data is currently not supported. data will have to be padded first if that is desired
+pub fn absorb_data<E,F:FnMut(&Position,&Position)->f32,I:IntoIterator<Item=Position>,T:FnMut(&E,&X)->f32,X>(data:&View<E>,mut movecost:F,offsetcandidates:I,mut transformcost:T,query:&View<X>)->Option<(Tensor<E>,f32)> where E:Clone{
+	let mut qx=Position::new(0);
+	let mut sx=Vec::new();
+
+	let offsetcandidates=offsetcandidates.into_iter();
+	let mut result:Option<(Tensor<(Position,f32)>,f32)>=None;
+
+	for offset in offsetcandidates{
+		let dims=query.dims();
+
+		sx.clear();
+		sx.extend((0..query.rank()).map(|n|{
+			let d=dims[n];
+			let o:usize=offset[n].try_into().unwrap();
+
+			o as isize..(d+o) as isize
+		}));
+
+		let mut acc=data.map_indexed(|e,ix|{
+			qx.clone_from(&ix);
+			qx-=&offset;
+
+			let cost=query.get(qx.as_unsigned()).map(|q|transformcost(e,q)).unwrap_or(f32::INFINITY);
+			(ix, cost)
+		});
+		propagate_cost(&mut acc,|ix,jx|{
+			qx.clone_from(&ix);
+			qx-=&offset;
+
+			query.get(qx.as_unsigned()).map(|q|movecost(&jx,&ix)+transformcost(&data[jx],q)).unwrap_or(f32::INFINITY)
+		});
+
+		assert!(acc.slice(&sx),"{sx:?}");
+
+		let cost:f32=acc.indices().map(|ix|acc[ix].1).sum();
+		if result.as_ref().map(|(_a,c)|cost<*c).unwrap_or(true){result=Some((acc,cost))}
+	}
+	result.map(|(a,c)|(a.map(|(position,_cost)|data[position].clone()),c))
+}
+/// find the lowest element greater than x
+pub fn ceil<'a,E:PartialOrd<E>+PartialOrd<X>,X>(e:&'a View<E>,x:&X)->Option<&'a E>{
+	let mut candidate=None;
+	for ix in e.indices(){
+		let e=&e[ix];
+		if e>x&&candidate.map(|c|c>e).unwrap_or(true){candidate=Some(e)}
+	}
+
+	candidate
+}
+/// find the lowest element greater than x
+pub fn ceil_index<'a,E:PartialOrd<E>+PartialOrd<X>,X>(e:&'a View<E>,x:&X)->Option<(&'a E,Position)>{
+	let mut candidate=None;
+	for ix in e.indices(){
+		let e=&e[&ix];
+		if e>x&&candidate.as_ref().map(|(c,_ix)|*c>e).unwrap_or(true){candidate=Some((e,ix))}
+	}
+
+	candidate
+}
+/// fills holes in the data using data from the left. holes with no left non holes will remain
+pub fn fill_holes<E,F:FnMut(&E)->E,G:FnMut(&E)->bool>(data:&mut View<E>,dim:isize,mut fill_hole:F,mut is_hole:G){
+	let data=data.swap_dims_mut(dim,-1);
+	let l=data.rank();
+	let mut left=Position::new(0);
+
+	for ix in data.indices(){
+		if is_hole(&data[&ix]){
+			if left.len()>0&&left[..l-1]==ix[..l-1]{data[&ix]=fill_hole(&data[&left])}
+		}
+		left.clone_from(&ix);
+	}
+}
+/// find the highest element less than x
+pub fn floor<'a,E:PartialOrd<E>+PartialOrd<X>,X>(e:&'a View<E>,x:&X)->Option<&'a E>{
+	let mut candidate=None;
+	for ix in e.indices(){
+		let e=&e[ix];
+		if e<x&&candidate.map(|c|c<e).unwrap_or(true){candidate=Some(e)}
+	}
+
+	candidate
+}
+/// find the highest element less than x
+pub fn floor_index<'a,E:PartialOrd<E>+PartialOrd<X>,X>(e:&'a View<E>,x:&X)->Option<(&'a E,Position)>{
+	let mut candidate=None;
+	for ix in e.indices(){
+		let e=&e[&ix];
+		if e<x&&candidate.as_ref().map(|(c,_ix)|*c<e).unwrap_or(true){candidate=Some((e,ix))}
+	}
+
+	candidate
+}
+/// grab lowest cost table with respect to the query from those inhabiting data slices from offsetcandidates with query dims. allowing the offset query to extend beyond the bounds of data is not supported; pad first if this is desired
+pub fn grab_table<'a,E,I:IntoIterator<Item=Position>,T:FnMut(&E,&X)->f32,X>(data:&'a View<E>,offsetcandidates:I,mut transform_cost:T,mut query:&View<X>)->Option<(&'a View<E>,f32)>{
+	assert!(data.rank()>=query.rank());
+	while data.rank()>query.rank(){query=query.unsqueeze_dim(0)}
+
+	let mut ix=Position::new(0);
+	let mut result:Option<(Position,f32)>=None;
+
+	for offset in offsetcandidates{
+		let cost=query.indices().fold(0.0,|acc,qx|{
+			ix.clone_from(&offset);
+			ix+=&qx;
+
+			acc+transform_cost(&data[&ix],&query[qx])
+		});
+
+		if result.as_ref().map(|(_bestoffset, bestcost)|*bestcost>cost).unwrap_or(true){result=Some((offset.clone(), cost))}
+	}
+	result.map(|(offset, cost)|{
+		let sx:Vec<Range<isize>>=offset.iter().zip(query.dims().iter())
+			.map(|(&ix,&qd)|ix..ix+qd as isize)
+			.collect();
+
+		(&data[sx.as_slice()],cost)
+	})
+}
+/// euclidean distance move cost TODO make this trait based so it works for tensors
+pub fn euclidean(p:&Position,q:&Position)->f32{p.iter().zip(q.iter()).map(|(p,q)|(p-q) as f32).map(|x|x*x).sum::<f32>().sqrt()}
+/// fuzzy equality based on which components match according to a predicate. broadcasting and rank promotion are performed, but not padding. dim mismatch after broadcasting and rank promotion will cause a result of false
+pub fn match_eq<E,F:FnMut(&E,&X)->bool,X>(mut e:&View<E>,mut f:F,mut x:&View<X>)->bool{
+	if e.dims().iter().rev().zip(x.dims().iter().rev()).any(|(&d,&x)|d!=x&&d!=1&&x!=1){return false}
+
+	while e.rank()<x.rank(){e=e.unsqueeze_dim(0)}
+	while e.rank()>x.rank(){x=x.unsqueeze_dim(0)}
+
+	let (edims,xdims)=(e.dims(),x.dims());
+	for n in 0..edims.len(){
+		if edims[n]==1{e=e.broadcast_dim(n as isize,xdims[n])}
+		if xdims[n]==1{x=x.broadcast_dim(n as isize,edims[n])}
+	}
+
+	e.indices().all(|ix|f(&e[&ix],&x[&ix]))
+}
+/// finds which indices match according to the predicate. broadcasting, padding, and rank promotion are performed // TODO those could be optional
+pub fn matches<'a,E,F:'a+FnMut(&E,&X)->bool,X>(mut e:&'a View<E>,mut f:F,mut x:&'a View<X>)->FilterMap<GridIter,impl 'a+FnMut(Position)->Option<Position>>{
+	while e.rank()<x.rank(){e=e.unsqueeze_dim(0)}
+	while e.rank()>x.rank(){x=x.unsqueeze_dim(0)}
+
+	let (edims,xdims)=(e.dims(),x.dims());
+	for n in 0..edims.len(){
+		if edims[n]==1{e=e.broadcast_dim(n as isize,xdims[n])}
+		if xdims[n]==1{x=x.broadcast_dim(n as isize,edims[n])}
+	}
+
+	let mut dims=edims.to_vec();
+	for n in 0..edims.len(){dims[n]=dims[n].max(xdims[n])}
+
+	GridIter::new(dims).filter_map(move|ix|f(e.get(&ix)?,x.get(&ix)?).then_some(ix))
+}
+/// propagate movement costs // TODO wrap args?
+pub fn propagate_cost<F:FnMut(&Position,&Position)->f32>(acc:&mut View<(Position,f32)>,mut cost:F){
+	let mut adjacent=Position::new(acc.rank());
+
+	for ix in acc.indices().chain(acc.indices().rev()){
+		adjacent.copy_from_slice(&ix);
+		for n in 0..adjacent.len(){
+			let dn=acc.dims()[n] as isize;
+
+			adjacent[n]=ix[n]-1;
+			let (jx,c)=if let Some((jx,_c))=(adjacent[n]>=0).then(||&acc[&adjacent]){(jx.clone(), cost(jx, &ix))}else{(ix.clone(), f32::INFINITY)};
+
+			adjacent[n]=ix[n]+1;
+			let (kx,d)=if let Some((kx,_c))=(adjacent[n]<dn).then(||&acc[&adjacent]){(kx.clone(), cost(kx, &ix))}else{(ix.clone(), f32::INFINITY)};
+
+			adjacent[n]=ix[n];
+			let (ix,b)=&mut acc[&ix];
+
+			if *b>c{(*ix,*b)=(jx,c)}
+			if *b>d{(*ix,*b)=(kx,d)}
+		}
+	}
+}
+use crate::builtin_tensor::{GridIter,Position,Tensor,View};
+use std::{cmp::PartialOrd,iter::FilterMap,ops::Range};
---
+++ umya_sheet.rs
@@ -0,0 +1,318 @@
+impl<E:Default,S:AsRef<Path>> LoadSheet<S> for IOResult<Tensor<E>> where Tensor<E>:for<'a> ReadSheet<&'a Spreadsheet>{
+	fn load_sheet(sheetpath:S)->Self{
+		let mut result=Ok(Default::default());
+
+		result.read_sheet(&mut [0,0,0],sheetpath.as_ref());
+		result
+	}
+}
+impl<E:Default,S:AsRef<Path>> LoadSheet<S> for Tensor<E> where Tensor<E>:for<'a> ReadSheet<&'a Path>{
+	fn load_sheet(sheetpath:S)->Self{Self::from_sheet(sheetpath.as_ref())}
+}
+impl<E:Default> From<Spreadsheet> for Tensor<E> where Tensor<E>:ReadSheet<Spreadsheet>{
+	fn from(sheet:Spreadsheet)->Tensor<E>{Self::from_sheet(sheet)}
+}
+impl<E:Default> From<Worksheet> for Tensor<E> where Tensor<E>:ReadSheet<Worksheet>{
+	fn from(sheet:Worksheet)->Tensor<E>{Self::from_sheet(sheet)}
+}
+impl<E:Default> ReadSheet<&Path> for IOResult<Tensor<E>> where Tensor<E>:for<'a> ReadSheet<&'a Spreadsheet>{
+	fn read_sheet(&mut self,indices:&mut [isize],sheet:&Path){
+		let x=match xlsx::read(sheet){Err(e)=>return *self=Err(IOError::new(IOErrorKind::Other,e)),Ok(x)=>x};
+		if let Ok(t)=self{return t.read_sheet(indices,&x)}
+
+		let mut t:Tensor<E>=Default::default();
+		t.read_sheet(indices,&x);
+
+		*self=Ok(t)
+	}
+}
+impl<E:Default> ReadSheet<&Path> for Tensor<E> where Tensor<E>:for<'a> ReadSheet<&'a Spreadsheet>{
+	fn read_sheet(&mut self,indices:&mut [isize],sheet:&Path){
+		let x=xlsx::read(sheet).unwrap();
+		self.read_sheet(indices,&x);
+	}
+}
+impl<E:Default> ReadSheet<&Spreadsheet> for Tensor<E> where Tensor<E>:for<'a> ReadSheet<&'a Worksheet>{
+	fn read_sheet(&mut self,indices:&mut [isize],sheet:&Spreadsheet){
+		assert!(indices.len()>=3);
+		let im3=indices.len()-3;
+
+		for n in 0..{
+			indices[im3]=n as isize;
+			self.read_sheet(indices,if let Some(s)=sheet.get_sheet(&n){s}else{break});
+		}
+	}
+}
+impl<E:Default> ReadSheet<Spreadsheet> for Tensor<E> where Tensor<E>:for<'a> ReadSheet<&'a Spreadsheet>{
+	fn read_sheet(&mut self,indices:&mut [isize],sheet:Spreadsheet){self.read_sheet(indices,&sheet)}
+}
+impl<E:Default> ReadSheet<Worksheet> for Tensor<E> where Tensor<E>:for<'a> ReadSheet<&'a Worksheet>{
+	fn read_sheet(&mut self,indices:&mut [isize],sheet:Worksheet){self.read_sheet(indices,&sheet)}
+}
+#[cfg(feature="match-tensor")]
+impl CellPattern{
+	/// builtin transformation cost function that uses levenstein*levscale for strings, xor*boolscale for bool, difference*diffscale for numbers, and parsescale when the string is not parsable
+	pub fn diff_lev_transform_cost<S:AsRef<str>>(&self,boolscale:f32,diffscale:f32,levscale:f32,parsescale:f32,s:S)->f32{
+		static METRIC:OnceLock<Levenshtein>=OnceLock::new();
+		let s=s.as_ref();
+
+		match self{
+			CellPattern::Anything=>0.0,
+			CellPattern::Bool(e)=>if let Ok(x)=s.parse::<bool>(){((e^x) as usize) as f32*boolscale}else{parsescale},
+			CellPattern::Boolean=>if let Ok(_x)=s.parse::<bool>(){0.0}else{parsescale},
+			CellPattern::Nothing=>if s.len()==0{0.0}else{parsescale},
+			CellPattern::Number(e)=>if let Ok(x)=s.parse::<f64>(){((e-x).abs()*diffscale as f64) as f32}else{parsescale},
+			CellPattern::Numeric=>if let Ok(_x)=s.parse::<f64>(){0.0}else{parsescale},
+			CellPattern::Text(z)=>METRIC.get_or_init(Levenshtein::new).distance(s,z) as f32*levscale
+		}
+	}
+}
+impl ReadSheet<&Worksheet> for Tensor<Option<Cell>>{
+	fn read_sheet(&mut self,indices:&mut [isize],sheet:&Worksheet){
+		assert!(indices.len()>=2);
+
+		let (xstop,ystop)=sheet.get_highest_column_and_row();
+		let im2=indices.len()-2;
+
+		while indices.len()>self.rank(){assert!(self.unsqueeze_dim(0))}
+
+		assert!(self.pad_dim(-2,xstop as usize,None)&&self.pad_dim(-1,ystop as usize,None));
+		if im2>0{assert!(self.pad_dim(-3,(indices[im2-1]+1) as usize,None))}
+
+		for x in 0..xstop{
+			indices[im2]=x as isize;
+			for y in 0..ystop{
+				indices[im2+1]=y as isize;
+				self[&indices[..]]=sheet.get_cell((x+1,y+1)).cloned();
+			}
+		}
+	}
+}
+impl ReadSheet<&Worksheet> for Tensor<String>{
+	fn read_sheet(&mut self,indices:&mut [isize],sheet:&Worksheet){
+		assert!(indices.len()>=2);
+
+		let (xstop,ystop)=sheet.get_highest_column_and_row();
+		let im2=indices.len()-2;
+
+		while indices.len()>self.rank(){assert!(self.unsqueeze_dim(0))}
+
+		assert!(self.pad_dim(-2,xstop as usize,String::new())&&self.pad_dim(-1,ystop as usize,String::new()));
+		if im2>0{assert!(self.pad_dim(-3,(indices[im2-1]+1) as usize,String::new()))}
+
+		for x in 0..xstop{
+			indices[im2]=x as isize;
+			for y in 0..ystop{
+				indices[im2+1]=y as isize;
+				self[&indices[..]]=sheet.get_value((x+1,y+1));
+			}
+		}
+	}
+}
+impl ReadSheet<&Worksheet> for Tensor<f32>{
+	fn read_sheet(&mut self,indices:&mut [isize],sheet:&Worksheet){
+		assert!(indices.len()>=2);
+
+		let (xstop,ystop)=sheet.get_highest_column_and_row();
+		let im2=indices.len()-2;
+
+		while indices.len()>self.rank(){assert!(self.unsqueeze_dim(0))}
+
+		assert!(self.pad_dim(-2,xstop as usize,f32::NAN)&&self.pad_dim(-1,ystop as usize,f32::NAN));
+		if im2>0{assert!(self.pad_dim(-3,(indices[im2-1]+1) as usize,f32::NAN))}
+
+		for x in 0..xstop{
+			indices[im2]=x as isize;
+			for y in 0..ystop{
+				indices[im2+1]=y as isize;
+				self[&indices[..]]=sheet.get_value_number((x+1,y+1)).unwrap_or(f64::NAN) as f32;
+			}
+		}
+	}
+}
+impl ReadSheet<&Worksheet> for Tensor<f64>{
+	fn read_sheet(&mut self,indices:&mut [isize],sheet:&Worksheet){
+		assert!(indices.len()>=2);
+
+		let (xstop,ystop)=sheet.get_highest_column_and_row();
+		let im2=indices.len()-2;
+
+		while indices.len()>self.rank(){assert!(self.unsqueeze_dim(0))}
+
+		assert!(self.pad_dim(-2,xstop as usize,f64::NAN)&&self.pad_dim(-1,ystop as usize,f64::NAN));
+		if im2>0{assert!(self.pad_dim(-3,(indices[im2-1]+1) as usize,f64::NAN))}
+
+		for x in 0..xstop{
+			indices[im2]=x as isize;
+			for y in 0..ystop{
+				indices[im2+1]=y as isize;
+				self[&indices[..]]=sheet.get_value_number((x+1,y+1)).unwrap_or(f64::NAN);
+			}
+		}
+	}
+}
+#[cfg(test)]
+mod tests{
+	#[cfg(feature="match-tensor")]
+	#[test]
+	fn match_matrix(){
+		use crate::match_tensor;
+
+		let expected=vec![80.0,80.0,80.0,75.0,75.0,70.0,70.0,65.0,70.0,65.0];
+		let matrixfile="Matrix November 10th FINAL.xlsx";
+		let numbers:Tensor<f32>=Tensor::load_sheet(matrixfile);
+
+		assert!(numbers[[0,0,0]].is_nan());
+		assert!(numbers[[0,0,1]].is_nan());
+		assert!(numbers[[0,1,0]].is_nan());
+
+		let mut query:Tensor<f32>=vec![70.0;expected.len()].into();
+		assert!(query.reshape([1,2,5]));
+
+		let offsetcandidates=numbers.indices().filter(|ix|ix[0]==1&&ix[1]<4&&ix[2]<4);
+		let (value, cost)=match_tensor::grab_table(&numbers,offsetcandidates,|a:&f32,b:&f32|{
+			if a.is_nan()&&b.is_nan(){0.0}else if a.is_nan()||b.is_nan(){100.0}else{(a-b).abs()}
+		},query.view()).unwrap();
+
+		assert_eq!(value.swap_dims(-1,-2).flat_vec(None),expected);
+		assert_eq!(cost,expected.into_iter().map(|e|(e-70.0).abs()).sum::<f32>());
+	}
+	#[cfg(feature="match-tensor")]
+	#[test]
+	fn find_table(){
+
+		let expected=vec![
+			"Single Investment Property",	"",						"",			"",						"",
+			"Maximum LTV/CLTV",				"",						">= 1.00",	"",						"",
+			"Minimum Credit Score",			"Maximum Loan Amount",	"Purchase",	"Rate/Term Refinance",	"Cash-Out Refinance",
+			"700",							"1000000",				"80",		"80",					"80*",
+			"",								"1500000",				"80",		"75",					"75",
+			"",								"2000000",				"75",		"70",					"70",
+			"",								"3000000",				"70",		"65",					"65",
+			"",								"3500000",				"70",		"65",					"NA",
+			"680",							"1000000",				"75",		"75",					"75",
+			"",								"1500000",				"75",		"75",					"75",
+			"",								"2000000",				"75",		"75",					"75",
+			"",								"2500000",				"70",		"65",					"65",
+			"",								"3000000",				"65",		"NA",					"NA",
+			"640",							"1000000",				"75",		"70",					"70",
+			"",								"1500000",				"70",		"70",					"70",
+			"",								"2000000",				"65",		"NA",					"NA",
+			"",								"3000000",				"60",		"NA",					"NA",
+		];
+		let matrixfile="Matrix November 10th FINAL.xlsx";
+		let pattern=vec![
+			CellPattern::Text("Single Investment Property".into()),CellPattern::Anything,CellPattern::Anything,CellPattern::Anything,CellPattern::Anything,
+			CellPattern::Text("Maximum LTV".into()),CellPattern::Anything,CellPattern::Text(">=1.0".into()),CellPattern::Anything,CellPattern::Anything,
+			CellPattern::Text("Minimum credit score".into()),CellPattern::Text("Maximum Loan Amount".into()),CellPattern::Text("Purchase".into()),CellPattern::Text("Rate/Term Refinance".into()),CellPattern::Text("Cash-Out Refinance".into()),
+			CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,
+			CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,
+			CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,
+			CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,
+			CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,
+			CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,
+			CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,
+			CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,
+			CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,
+			CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,
+			CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,
+			CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,
+			CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,
+			CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,CellPattern::Numeric,
+		];
+
+		let mut data:Tensor<String>=Tensor::load_sheet(matrixfile);
+		let mut pattern:Tensor<CellPattern>=Tensor::from(pattern);
+
+		assert!(data.slice(&[0..3,0..5,0..30]));
+		assert!(pattern.reshape([17,5]));
+		assert!(pattern.swap_dims(0,1));
+
+		let (table, _cost)=grab_table_default(data,pattern);
+
+		assert_eq!(table.view().swap_dims(-1,-2).flat_vec(None),expected);
+	}
+	#[test]
+	fn read_matrix(){
+		let matrixfile="Matrix November 10th FINAL.xlsx";
+		let numbers:Tensor<f32>=Tensor::load_sheet(matrixfile);
+		let text:Tensor<String>=Tensor::load_sheet(matrixfile);
+
+		assert!(numbers[[0,0,0]].is_nan());
+		assert!(numbers[[0,0,1]].is_nan());
+		assert!(numbers[[0,1,0]].is_nan());
+		assert_eq!(numbers[[1..2,2..4,3..8].as_slice()].swap_dims(-1,-2).flat_vec(None),vec![80.0,80.0,80.0,75.0,75.0,70.0,70.0,65.0,70.0,65.0]);
+		assert_eq!(text[[0,0,1]],"test");
+	}
+	use super::*;
+}
+#[cfg(feature="match-tensor")]
+#[derive(Clone,Debug,Default,PartialEq)]
+#[cfg(feature="match-tensor")]
+/// cell type usable for pattern matching
+pub enum CellPattern{
+	#[default]
+	/// any value
+	Anything,
+	/// parable bool equal to
+	Bool(bool),
+	/// parsable bool
+	Boolean,
+	/// specifically an empty cell
+	Nothing,
+	/// parsable number equal to
+	Number(f64),
+	/// parsable number
+	Numeric,
+	/// text equal to
+	Text(String)
+}
+#[cfg(feature="match-tensor")]
+/// fuzzy finds a table based on the cell pattern
+pub fn grab_table(data:impl AsRef<View<String>>,boolscale:f32,diffscale:f32,levscale:f32,parsescale:f32,pattern:impl AsRef<View<CellPattern>>)->(Tensor<String>,f32){
+	let (data,mut pattern)=(data.as_ref(),pattern.as_ref());
+
+	assert!(data.rank()>=pattern.rank());
+	while data.rank()>pattern.rank(){pattern=pattern.unsqueeze_dim(0)}
+
+	let (datadims,patterndims)=(data.dims(),pattern.dims());
+	let mut stopposition=vec![0;datadims.len()];
+
+	datadims.iter().zip(patterndims.iter()).zip(stopposition.iter_mut()).for_each(|((d,p),s)|*s=d.checked_sub(*p).unwrap()+1);
+	let offsetcandidates=GridIter::new(stopposition);
+
+	match_tensor::grab_table(data,offsetcandidates,|d,p|p.diff_lev_transform_cost(boolscale,diffscale,levscale,parsescale,d),pattern).map(|(t, c)|(t.to_tensor(), c)).unwrap()
+}
+#[cfg(feature="match-tensor")]
+/// fuzzy finds a table based on the cell pattern
+pub fn grab_table_default(data:impl AsRef<View<String>>,pattern:impl AsRef<View<CellPattern>>)->(Tensor<String>,f32){grab_table(data,1.0,1.0,1.0,5.0,pattern)}
+/// provide function for loading spreadsheet to tensor by path refs
+pub trait LoadSheet<S>{
+	/// loads a tensor from the spreadsheet file
+	fn load_sheet(sheetpath:S)->Self;
+}
+/// how to read the columns of a spreadsheet into a tensor
+pub trait ReadSheet<S>{
+	/// reads the excel sheet into a tensor
+	fn from_sheet(sheet:S)->Self where Self:Default{
+		let mut tensor=Self::default();
+
+		tensor.read_sheet(&mut [0,0,0],sheet);
+		tensor
+	}
+	/// reads the tensor from excel format
+	fn read_sheet(&mut self,indices:&mut [isize],sheet:S);
+}
+#[cfg(feature="match-tensor")]
+use b_k_tree::{metrics::Levenshtein,DiscreteMetric};
+use crate::builtin_tensor::Tensor;
+#[cfg(feature="match-tensor")]
+use crate::{
+	builtin_tensor::{GridIter,View},match_tensor
+};
+use std::{
+	io::{Error as IOError,ErrorKind as IOErrorKind,Result as IOResult},path::Path
+};
+#[cfg(feature="match-tensor")]
+use std::sync::OnceLock;
+use umya_spreadsheet::{Cell,Spreadsheet,Worksheet,reader::xlsx};
