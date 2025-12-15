e015b2f4cac540e13eb19e87af464d5cbc91e71e
Aili Nierengarten <125620224+8000thCube@users.noreply.github.com>
2025-12-15 04:58:13 -0800

    Update match_tensor.rs

--- src/match_tensor.rs
+++ src/match_tensor.rs
@@ -88,7 +88,7 @@ pub fn fill_holes<E,F:FnMut(&E)->E,G:FnMut(&E)->bool>(data:&mut View<E>,dim:isiz
 
 	for ix in data.indices(){
 		if is_hole(&data[&ix]){
-			if left.len()>0&&left[0]==ix[0]{data[&ix]=fill_hole(&data[&left])}
+			if left.len()>0&&left[1..]==ix[1..]{data[&ix]=fill_hole(&data[&left])}
 		}else{
 			left.clone_from(&ix);
 		}
